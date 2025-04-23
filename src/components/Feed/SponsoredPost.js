26ccc0dc83571aed4e94a094043d299e7d23f5b5
prangya244 <your.email@example.com>
2025-04-23 12:43:51 +0530

    comments in addlike added

--- src/components/Feed/SponsoredPost.js
+++ src/components/Feed/SponsoredPost.js
@@ -43,7 +43,7 @@ const SponsoredPost = ({ postId, email }) => {
         POST: String(postId),
         LIKES: updatedLikes,
         NOACTION: false,
-        CUST1: 'NA',
+        CUST1: comments?comments:'NA',
         CUST2: 'NA',
         CUST3: 'NA',
       };
